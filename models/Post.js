7aabcaa2c9c73e3cc963d7ece9bea9944f82fe33
KruthikaSuresh <64856242+KruthikaSuresh@users.noreply.github.com>
2020-12-03 19:29:50 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -24,6 +24,14 @@ const PostSchema = new Schema({
             }
         }
     ],
+    requests: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
     comments: [
         {
             user: {
